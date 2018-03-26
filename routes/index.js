35ed713654014b236b754b0f765f6db94104d08c
Devanshu Mehta <devanshumehta95@gmail.com>
2018-03-26 15:05:18 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('product', { title: 'Products' });
+
 });
 
 router.get("/cart.ejs", function(req, res){
-    res.render("cart");
+    res.render("cart", { title: 'Shopping Cart' });
 });
 
 router.get("/checkout.ejs", function(req, res){
-    res.render("checkout");
+    res.render("checkout", { title: 'Checkout' });
 });
 
 router.get("/product.ejs", function(req, res){
-    res.render("product");
+    res.render("product", { title: 'Products' });
 });
 
 router.get("/confirmation.ejs", function(req, res){
-    res.render("confirmation");
+    res.render("confirmation", { title: 'Order Confirmation' });
 });
 
 module.exports = router;
